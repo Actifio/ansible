e38cac4d295efc946b5c3456cced5a8ab64ae4c5
Michael Chew <michael.chew@actifio.com>
2018-11-16 08:59:09 +1100

    Update getjobs.sh

--- getjobs/getjobs.sh
+++ getjobs/getjobs.sh
@@ -9,7 +9,7 @@ export ANSIBLE_NOCOWS=1
 # Use the -e, or --extra-vars argument of ansible-playbook
 
 if [ "$1" = "" ] ; then
-  ansible-playbook -i ./hosts ./getjobs.yml --extra-vars "acthost=melnabsky" --key-file /Users/michael/.ssh/johndoe-v2-key 
+  ansible-playbook -i ./hosts ./getjobs.yml --extra-vars "acthost=melnabsky" --key-file /Users/johndoe/.ssh/johndoe-v2-key 
 else
-  ansible-playbook -i ./hosts ./getjobs.yml --extra-vars "acthost=melnabsky job_id=$1" --key-file /Users/michael/.ssh/johndoe-v2-key 	
+  ansible-playbook -i ./hosts ./getjobs.yml --extra-vars "acthost=melnabsky job_id=$1" --key-file /Users/johndoe/.ssh/johndoe-v2-key 	
 fi
