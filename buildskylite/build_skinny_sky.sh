fca478ddcf4ac3f6e3eeed7c1e9983f36bd192ef
Michael Chew <michael.chew@actifio.com>
2018-11-16 09:00:25 +1100

    Update build_skinny_sky.sh

--- buildskylite/build_skinny_sky.sh
+++ buildskylite/build_skinny_sky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##
-# File: build_skinny.sh
+# File: build_skinny_sky.sh
 ##
 
 export ANSIBLE_NOCOWS=1
@@ -9,7 +9,7 @@ export ANSIBLE_NOCOWS=1
 # Use the -e, or --extra-vars argument of ansible-playbook
 
 if [ "$1" = "" ] ; then
-  ansible-playbook -i ./hosts ./build_skinny_sky.yml --extra-vars "acthost=sky811" --key-file /Users/michael/.ssh/michaelchew-v2-key 
+  ansible-playbook -i ./hosts ./build_skinny_sky.yml --extra-vars "acthost=sky811" --key-file /Users/johndoe/.ssh/johndoe-v2-key 
 else
-  ansible-playbook -i ./hosts ./build_skinny_sky.yml --extra-vars "acthost=sky811 cluster_name=$1" --key-file /Users/michael/.ssh/michaelchew-v2-key 
+  ansible-playbook -i ./hosts ./build_skinny_sky.yml --extra-vars "acthost=sky811 cluster_name=$1" --key-file /Users/johndoe/.ssh/johndoe-v2-key 
 fi
